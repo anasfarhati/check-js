380ba5814d0331fde77cae55c3cc900cb4ec657d
anass <anasfarhati84@gmail.com>
2023-08-12 09:56:34 +0100

    adding react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import React from 'react';
+import { Container, Navbar, Nav, Card, Row, Col } from 'react-bootstrap';
 
 function App() {
   return (
+    <React.Fragment>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">My Bootstrap App</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+        <Container className="mt-4">
+          <h1> My App</h1>
+          <Row>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card 1</Card.Title>
+                  <Card.Text>This is the content of Card 1.</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card 2</Card.Title>
+                  <Card.Text>This is the content of Card 2.</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <Card.Title>Card 3</Card.Title>
+                  <Card.Text>This is the content of Card 3.</Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
     </div>
+    </React.Fragment>
   );
 }
 
